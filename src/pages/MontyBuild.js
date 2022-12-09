8dc5a14d4147c8cc08dbf6da31d1b75726f8b10a
terrence <terrence.budnik@gmail.com>
2022-12-08 23:30:35 -0600

    added more css

--- src/pages/MontyBuild.js
+++ src/pages/MontyBuild.js
@@ -12,6 +12,7 @@ export default function MontyBuild() {
 
         <Container className='home'>
             <div>
+                
                 <figure className='head'>
                     <div className='eyes'>
                         <span id='eye1'></span>
@@ -19,6 +20,17 @@ export default function MontyBuild() {
                     </div>
                 </figure>
                 <figure className='body'></figure>
+                <figure className='legs'>
+                    <span id='leg1'></span>
+                    <span id='leg2'></span>
+                    <span id='leg3'></span>
+                    <span id='leg4'></span>
+                    <span id='leg5'></span>
+                    <span id='leg6'></span>
+                    <span id='leg7'></span>
+                    <span id='leg8'></span>
+                </figure>
+
             </div>
         </Container>
 
