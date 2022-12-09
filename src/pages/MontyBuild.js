34c574f1233ac1ea2767825f75968903b8e67adb
terrence <terrence.budnik@gmail.com>
2022-12-08 23:12:46 -0600

    added css to build page

--- src/pages/MontyBuild.js
+++ src/pages/MontyBuild.js
@@ -3,13 +3,23 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import '../css/montybuild.css'
+
 
 export default function MontyBuild() {
 
     return (
 
-        <Container>
-            
+        <Container className='home'>
+            <div>
+                <figure className='head'>
+                    <div className='eyes'>
+                        <span id='eye1'></span>
+                        <span id='eye2'></span>
+                    </div>
+                </figure>
+                <figure className='body'></figure>
+            </div>
         </Container>
 
     );
