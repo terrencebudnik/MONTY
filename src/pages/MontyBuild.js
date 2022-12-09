f2ee0e0c93a892a572f71156b80113a6be9bcd2b
terrence <terrence.budnik@gmail.com>
2022-12-09 01:08:39 -0600

    added x, y, and z animations

--- src/pages/MontyBuild.js
+++ src/pages/MontyBuild.js
@@ -1,37 +1,42 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+import Draggable from 'react-draggable';
 import '../css/montybuild.css'
 
 
 export default function MontyBuild() {
-
+   
     return (
-
         <Container className='home'>
-            <div>
-                
-                <figure className='head'>
-                    <div className='eyes'>
-                        <span id='eye1'></span>
-                        <span id='eye2'></span>
-                    </div>
-                </figure>
-                <figure className='body'></figure>
-                <figure className='legs'>
-                    <span id='leg1'></span>
-                    <span id='leg2'></span>
-                    <span id='leg3'></span>
-                    <span id='leg4'></span>
-                    <span id='leg5'></span>
-                    <span id='leg6'></span>
-                    <span id='leg7'></span>
-                    <span id='leg8'></span>
-                </figure>
+            <div className='moveXEl'>
 
+            <div className='moveYEl'>
+         
+                <div className ='monty'>
+
+                    <figure className='head'>
+                        <div className='eyes'>
+                            <span id='eye1'></span>
+                            <span id='eye2'></span>
+                        </div>
+                    </figure>
+                    <figure className='body'></figure>
+                    <figure className='legs'>
+                        <span id='leg1'></span>
+                        <span id='leg2'></span>
+                        <span id='leg3'></span>
+                        <span id='leg4'></span>
+                        <span id='leg5'></span>
+                        <span id='leg6'></span>
+                        <span id='leg7'></span>
+                        <span id='leg8'></span>
+                    </figure>
+
+                </div>
+               
             </div>
+        </div>
+          
         </Container>
 
     );
